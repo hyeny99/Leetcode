3f62660b9ec5c2b1f7f4783226bfe07bdfeb0135
Hyeny Jeon <60022132+hyeny99@users.noreply.github.com>
2021-05-12 16:52:52 -0400

    Update NumMatrix.java

--- java/NumMatrix.java
+++ java/NumMatrix.java
@@ -1,12 +1,29 @@
-
 public class NumMatrix {
 
-    public NumMatrix(int[][] matrix) {
-        
-    }
-    
-    public int sumRegion(int row1, int col1, int row2, int col2) {
-        
-    }
+	int[][] matrix;
+
+	public NumMatrix(int[][] matrix) {
+		this.matrix = matrix;
+	}
+
+	public int sumRegion(int row1, int col1, int row2, int col2) {
+		int sum = 0;
+		
+	
+
+		while (row1 <= row2) {
+			int colstart = col1;
+			while (colstart <= col2) {
+				sum += matrix[row1][colstart];
+				colstart++;
+			}
+
+			
+			row1++;
+		}
+		
+		return sum;
+
+	}
 
 }
